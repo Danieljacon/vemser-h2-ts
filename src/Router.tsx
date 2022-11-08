65e902ed01e60c8da141a1b1f2d50d5f301de48e
Danieljacon <danielcamillo2020@gmail.com>
2022-11-08 18:41:13 -0300

    Estrutura inicial

---
+++ src/Router.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+
+export const Router: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<div>Not Found</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
